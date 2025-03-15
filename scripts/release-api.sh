86041fcd71bb81900e05317a1d18189661cf39ea
Asaf Shakarzy <asaf000@gmail.com>
2025-03-15 15:06:55 +0200

    chore: api changes

--- scripts/release-api.sh
+++ scripts/release-api.sh
@@ -22,7 +22,9 @@ popd >/dev/null || exit
 pushd "packages/$SERVICE_NAME" >/dev/null || exit
 
 printf '%s\n' "Releasing $SERVICE_NAME..."
-"$VIRTUAL_ENV/bin/semantic-release" -v version --no-push
+"$VIRTUAL_ENV/bin/semantic-release" -v version
+
+"$VIRTUAL_ENV/bin/semantic-release" -v publish
 
 # printf '%s\n' "Writing changelog for $SERVICE_NAME..."
 # "$VIRTUAL_ENV/bin/semantic-release" -v changelog
