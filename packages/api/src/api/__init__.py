18ba321100df483606c495febfc7368f470dc61e
Asaf Shakarzy <asaf000@gmail.com>
2025-03-15 19:23:32 +0200

    fix(api): bumping api to 0.2.12

--- packages/api/src/api/__init__.py
+++ packages/api/src/api/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.2.11"
 
 
 def main() -> None:
-    print("Hello from api, expecting v0.2.11 with small change!!! :)")
+    print("Hello from api, expecting v0.2.12 with small change!!! :)")
