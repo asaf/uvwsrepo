ff7914e34c4b5451423dd0841aea8dec9af46785
Asaf Shakarzy <asaf000@gmail.com>
2025-03-15 17:52:43 +0200

    fix(api): 0.2.2 change

--- packages/api/src/api/__init__.py
+++ packages/api/src/api/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.2.1"
 
 
 def main() -> None:
-    print("Hello from api, expecting v0.2.1 with small change!! :)")
+    print("Hello from api, expecting v0.2.2 with small change!! :)")
