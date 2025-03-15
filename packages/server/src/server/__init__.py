f5262e512780f6b63a81a37a7672a70112e62065
Asaf Shakarzy <asaf000@gmail.com>
2025-03-15 17:43:10 +0200

    fix(server): server goes minor

--- packages/server/src/server/__init__.py
+++ packages/server/src/server/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.2.1"
 
 
 def main() -> None:
-    print("Hello from server!!!! expecting to be in v0.2.1 coz it's a small change!")
+    print("Hello from server!!!! expecting to be in v0.2.2 coz it's a small change!")
