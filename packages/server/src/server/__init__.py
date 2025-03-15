3fdf8b85206c66375151e1309f26ecb17b528009
Asaf Shakarzy <asaf000@gmail.com>
2025-03-15 18:54:29 +0200

    fix(server): a server change

--- packages/server/src/server/__init__.py
+++ packages/server/src/server/__init__.py
@@ -2,4 +2,4 @@ __version__ = "0.2.3"
 
 
 def main() -> None:
-    print("Hello from server!!!! expecting to be in v0.2.3 coz it's a small change!")
+    print("Hello from server!!!! expecting to be in v0.2.4 coz it's a small change!")
